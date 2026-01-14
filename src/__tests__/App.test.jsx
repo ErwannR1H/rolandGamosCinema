cdc41cf0304d0a53e92fd5107b3a6980699c48ae
Erwann HEQUET <hequet.erwann@gmail.com>
2026-01-14 09:29:26 +0100

    test du blocage du merge en cas de test failed

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -40,4 +40,11 @@ describe('App', () => {
     const aboutLink = screen.getByText(/Vers analyse de data/i).closest('a');
     expect(aboutLink).toHaveAttribute('href', '/about');
   });
+
+  test('DOIT FAIL', () => {
+    render(<App />);
+    
+    const aboutLink = screen.getByText(/Vers analyse de data/i).closest('a');
+    expect(aboutLink).toHaveAttribute('href', '/FAILLL');
+  });
 });
