c934d70f8424f835bbcdfa87a365431a94d248fa
Erwann HEQUET <hequet.erwann@gmail.com>
2026-01-14 08:54:24 +0100

    Ajout CI

--- jest.config.js
+++ jest.config.js
@@ -10,4 +10,17 @@ export default {
   testMatch: [
     '<rootDir>/src/**/__tests__/**/*.test.{js,jsx}',
   ],
+  collectCoverageFrom: [
+    'src/components/**/*.{js,jsx}',
+    '!src/**/__tests__/**',
+    '!src/**/*.test.{js,jsx}'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 85,
+      functions: 75,
+      lines: 90,
+      statements: 90
+    }
+  }
 };
